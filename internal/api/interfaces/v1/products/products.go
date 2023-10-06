c24cfd183f5f8ba855b9eb41ebc3d19926199168
Takayuki Yorikane <tyorikan@gmail.com>
2023-10-06 16:35:26 +0900

    Add product detail endpoint and return lowercase keys

--- internal/api/interfaces/v1/products/products.go
+++ internal/api/interfaces/v1/products/products.go
@@ -21,20 +21,40 @@ func NewProductResources() *ProductResources {
 func (rs ProductResources) Routes() chi.Router {
 	r := chi.NewRouter()
 	r.With().Get("/", rs.list)
+	r.With().Get("/{productId}", rs.getDetail)
 	return r
 }
 
 type Product struct {
-	ID    int
-	Name  string
-	Price float64
+	ID    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type ProductDetail struct {
+	Product
+	Description string `json:"description"`
 }
 
 func (rs ProductResources) list(w http.ResponseWriter, r *http.Request) {
 	products := []Product{
-		{ID: 1, Name: "Product 1", Price: 100.0},
-		{ID: 2, Name: "Product 2", Price: 200.0},
+		{ID: "1111", Name: "Product 1", Price: 100.0},
+		{ID: "2222", Name: "Product 2", Price: 200.0},
 	}
 	logrus.Debug(products)
 	helper.Succeed(w, products)
+}
+
+func (rs ProductResources) getDetail(w http.ResponseWriter, r *http.Request) {
+	productID := chi.URLParam(r, "productId")
+	product := ProductDetail{
+		Product: Product{
+			ID:    productID,
+			Name:  "Product " + productID,
+			Price: 100.0,
+		},
+		Description: "This is a great collaboration product!",
+	}
+	logrus.Debug(product)
+	helper.Succeed(w, product)
 }
